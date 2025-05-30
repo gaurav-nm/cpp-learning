a75492a53da8fd0722a8bd1532b6153de0d8a630
Gaurav <gauravpalstudy@gmail.com>
2025-05-30 07:52:19 +0530

    add file for conditionals and loop

---
+++ if.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+/*
+int main() {
+    int a;
+    cin >> a;
+
+    int n = a;
+    cout << "Value of n is: " << n << endl;
+
+    if (a > 0)
+        cout << "The number is positive" << endl;
+    else
+        cout << "Number a is negative" << endl;
+
+    return 0;
+}
+*/
+
+
+int main(){
+    
+}
+
+
