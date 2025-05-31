78500603d75f409927b33d44ece5b27242764368
Gaurav <gauravpalstudy@gmail.com>
2025-05-31 07:20:41 +0530

    move all file in folder syatem

---
+++ Lec3 if else while loop lots of pattern/31mayquestions.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+/* solve the question for 
+char ch
+from (a) to (z) : print "this is lower case"
+from (A)to (Z) : print "this is upper case "
+from (0) to (9) : print "this is numeric value"
+*/
+int main(){
+    
+
+}
