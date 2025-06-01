f9c627fea5e2206e62a85e2239af52248a968ff3
Gaurav <gauravpalstudy@gmail.com>
2025-06-01 15:48:44 +0530

    add pattern based questions

---
+++ Lec4 Pattern/pattern.cpp
@@ -0,0 +1,80 @@
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n ;
+
+    int i =1 ;
+    while(i<=n){
+        int j =1;
+        while (j<=n)
+        {
+            cout<<n-j+1;
+            j=j+1;
+
+        }
+        cout<<endl;
+        i=i+1;
+        
+    }
+
+}
+
+*/
+
+/* Question 2 
+123
+456
+789
+
+point to remember : int count add krna imp h 
+*/
+
+/*
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n ;
+    cin >> n;
+    
+
+
+    int i=1;
+    int count= 1 ;
+
+    while(i<=n){
+        int j=1;
+        while (j<=n){
+            cout<<count << " ";
+            count = count +1;
+            j=j+1;
+
+        }
+        cout << endl;
+        i=i+1;
+
+    }
+}
+
+*/
+
+
+
+/// question 4
+
+/// jitne i utne hi j 
+/*
+*
+**
+***
+****
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+}
